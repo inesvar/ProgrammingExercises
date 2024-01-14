23c807ce3ce747d32e2a6a688929f7fe8a1efd1a
inesvar <inesvar 90340779+inesvar@users.noreply.github.com>
2024-01-14 21:42:48 +0100

    Codingame "Code vs Zombie" challenge [to debug]

---
+++ Codingame/zombies.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <list>
+
+using namespace std;
+
+const int LARG = 15999;
+const int HAUT = 8999;
+const unsigned MCT_DEPTH = 0;
+
+unsigned fibonacci(unsigned n) {
+    if (n <= 1)  {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+unsigned fibonacciSum(unsigned n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return fibonacciSum(n - 1) + fibonacci(n);
+    }
+}
+
+unsigned compute_score(unsigned kills, unsigned human_count) {
+    unsigned score = 10 * pow(human_count, 2);
+    score *= fibonacciSum(kills);
+    cerr << "score " << score << "\n";
+    return score;
+}
+
+class Playground {
+    public:
+        list<pair<int, int>> human_pos{};
+        vector<pair<int, int>> zombie_pos{};
+        vector<pair<int, int>> next_moves{};
+        vector<unsigned> score{};
+        pair<int, int> pos{};
+        unsigned human_count{};
+
+    void clear() {
+        human_pos.clear();
+        zombie_pos.clear();
+        next_moves.clear();
+    }
+
+    void move() {
+        if (human_count > 0) {
+            unsigned i = compute_most_rewarding_move(MCT_DEPTH).first;
+            pos = next_moves[i];
+        } else {
+            pos = *human_pos.begin();
+            /* pair<int, int> human_mean = pair(0, 0);
+            pair<int, int> zombie_mean = pair(0, 0);
+            unsigned count = 0;
+            for (auto h = human_pos.begin(); h != human_pos.end(); h++) {
+                human_mean.first += h->first;
+                human_mean.second += h->second;
+                count++;
+            }
+            pos.first = human_mean.first / count * 0.5;
+            pos.second = human_mean.second / count * 0.5;
+            count = 0;
+            for (auto h = zombie_pos.begin(); h != zombie_pos.end(); h++) {
+                zombie_mean.first += h->first;
+                zombie_mean.second += h->second;
+                count++;
+            }
+            pos.first += zombie_mean.first / count * 0.5;
+            pos.second += zombie_mean.second / count * 0.5; */
+        }
+    }
+
+    void compute_possible_moves() {
+        next_moves.emplace_back(min(LARG, pos.first + 1000), pos.second);
+        next_moves.emplace_back(pos.first, max(0, pos.second - 1000));
+        next_moves.emplace_back(max(0, pos.first - 1000), pos.second);
+        next_moves.emplace_back(pos.first, min(HAUT, pos.second + 1000));
+    }
+
+    pair<unsigned, unsigned> compute_most_rewarding_move(unsigned depth) {
+        compute_possible_moves();
+        Playground *next_pg = new Playground();
+        for (int i = 0; i < 4; i++) {
+            cerr << "depth " << depth << " move " << i << "\n";
+            
+            next_pg->pos = next_moves[i];
+            cerr << "pos " << next_pg->pos.first << " " << next_pg->pos.second << "\n";
+
+            score.push_back(compute_score(next_pg->attack_zombies(zombie_pos),
+                                          human_count));
+
+            next_pg[i].zombies_eat(human_pos);
+
+            if (depth > 0) {
+                score[i] += next_pg->compute_most_rewarding_move(depth - 1).second;
+            }
+        }
+
+        return get_max_score();
+    }
+
+    unsigned attack_zombies(vector<pair<int, int>> &old_zombie_pos) {
+        unsigned zombies_killed = 0;
+        for (auto z = old_zombie_pos.begin(); z != old_zombie_pos.end(); z++) {
+            if (pow(z->first - pos.first, 2) +
+                pow(z->second - pos.second, 2) >= 4000000) {
+                    zombie_pos.emplace_back(z->first, z->second);
+                    cerr << "zombie alive at " << z->first << " " << z->second << "\n";
+            } else {
+                zombies_killed += 1;
+                cerr << "zombies killed at " << z->first << " " << z->second << "\n";
+            }
+        }
+        cerr << "zombies killed " << zombies_killed << "\n";
+        return zombies_killed;
+    }
+
+    void zombies_eat(list<pair<int, int>> &old_human_pos) {
+        cerr << "zombies eat" << "\n";
+        // FIXME : this doesn't work as expected
+        cerr << "zombie at " << zombie_pos.begin() << " " << zombie_pos.end() << "\n";
+        for (auto z = zombie_pos.begin(); z != zombie_pos.end(); z++) {
+            for (auto h = old_human_pos.begin(); h != old_human_pos.end();) {
+                if (z->first == h->first && z->second == h->second) {
+                    cerr << "human eaten at " << h->first << " " << h->second << "\n"; 
+                    cerr << "human eaten at " << z->first << " " << z->second << "\n"; 
+                    h = old_human_pos.erase(h);
+                } else {
+                    h++;
+                }
+            }
+        }
+        for (auto h = old_human_pos.begin(); h != old_human_pos.end(); h++) {
+            human_pos.emplace_back(h->first, h->second);
+            human_count += 1;
+        }
+        cerr << "human count " << human_count << "\n"; 
+    }
+
+    pair<unsigned, unsigned> get_max_score() const {
+        auto i = max_element(score.begin(), score.end());
+        return pair(distance(score.begin(), i), *i);
+    }
+};
+
+int main()
+{
+    Playground *pg = new Playground();
+
+    // game loop
+    while (1) {
+        pg->clear();
+        cin >> pg->pos.first >> pg->pos.second; cin.ignore();
+        cin >> pg->human_count; cin.ignore();
+        for (int i = 0; i < pg->human_count; i++) {
+            int human_id;
+            int human_x;
+            int human_y;
+            cin >> human_id >> human_x >> human_y; cin.ignore();
+            cerr << "human at " << human_x << " " << human_y << "\n";
+            pg->human_pos.emplace_back(human_x, human_y);
+        }
+
+        int zombie_count;
+        cin >> zombie_count; cin.ignore();
+        for (int i = 0; i < zombie_count; i++) {
+            int zombie_id;
+            int zombie_x;
+            int zombie_y;
+            int zombie_xnext;
+            int zombie_ynext;
+            cin >> zombie_id >> zombie_x >> zombie_y >> zombie_xnext >> zombie_ynext; cin.ignore();
+            cerr << "zombie at " << zombie_xnext << " " << zombie_ynext << "\n";
+            pg->zombie_pos.emplace_back(zombie_xnext, zombie_ynext);
+        }
+
+        pg->move();
+
+        cout << pg->pos.first << " " << pg->pos.second << endl; // Your destination coordinates
+    }
+}
