974a04836a4757653813e9da07bf371896d88d0d
inesvar <inesvar 90340779+inesvar@users.noreply.github.com>
2024-01-21 16:30:45 +0100

    Zombies now move + score takes into account game over
    To do next :
    - restrain possible moves to interesting zones in the playground
    - compute multiple possibilities only when the zone is in range
    - figure out if N/E/S/W directions are still needed, and if yes,
    implement a better tree using `possibilities_tree.png`
    
    To do later :
    - compute the humans' minimal life duration and time to reach

--- Codingame/zombies.cpp
+++ Codingame/zombies.cpp
@@ -10,6 +10,10 @@ using namespace std;
 const int LARG = 15999;
 const int HAUT = 8999;
 const unsigned MCT_DEPTH = 2;
+const int SPEED = 1000;
+const int GUN_RANGE_SQUARED = 4000000;
+const int ZOMBIE_SPEED = 400;
+const int GAMEOVER = -10000;
 
 unsigned fibonacci(unsigned n) {
     if (n <= 1)  {
@@ -27,10 +31,10 @@ unsigned fibonacciSum(unsigned n) {
     }
 }
 
-unsigned compute_score(unsigned kills, unsigned human_count) {
-    unsigned score = 10 * pow(human_count, 2);
+int compute_score(unsigned kills, unsigned human_count) {
+    int score = 10 * pow(human_count, 2);
     score *= fibonacciSum(kills);
-    cerr << "score " << score << "\n";
+    // cerr << "score " << score << " ";
     return score;
 }
 
@@ -41,24 +45,30 @@ void print(pair<int, int> p, bool end_of_line = true) {
     }
 }
 
-void print(vector<pair<int, int>> &v) {
+void print(vector<pair<int, int>> &v, bool end_of_line = true) {
     cerr << "[ ";
     for (auto i = v.begin(); i != v.end(); i++) {
         cerr << "(";
         print(*i, false);
         cerr << "), ";
     }
-    cerr << " ]\n";
+    cerr << " ]";
+    if (end_of_line) {
+        cerr << "\n";
+    }
 }
 
-void print(list<pair<int, int>> &v) {
+void print(list<pair<int, int>> &v, bool end_of_line = true) {
     cerr << "[ ";
     for (auto i = v.begin(); i != v.end(); i++) {
         cerr << "(";
         print(*i, false);
         cerr << ")";
     }
-    cerr << " ]\n";
+    cerr << " ]";
+    if (end_of_line) {
+        cerr << "\n";
+    }
 }
 
 class Playground {
@@ -66,7 +76,7 @@ class Playground {
         list<pair<int, int>> human_pos{};
         vector<pair<int, int>> zombie_pos{};
         vector<pair<int, int>> next_moves{};
-        vector<unsigned> score{};
+        vector<int> score{};
         pair<int, int> pos{};
         unsigned human_count{};
 
@@ -80,6 +90,8 @@ class Playground {
         if (human_count > 0) {
             unsigned i = compute_most_rewarding_move(MCT_DEPTH).first;
             pos = next_moves[i];
+            cerr << "\n\nMOVED TO ";
+            print(pos);
         } else {
             pos = *human_pos.begin();
             /* pair<int, int> human_mean = pair(0, 0);
@@ -120,20 +132,26 @@ class Playground {
 
     pair<unsigned, unsigned> compute_most_rewarding_move(unsigned depth) {
         compute_possible_moves();
-        for (int i = 0; i < next_moves.size(); i++) {
+        for (unsigned i = 0; i < next_moves.size(); i++) {
             Playground *next_pg = new Playground();
-            cerr << "\ndepth " << depth << " move " << i << "\n";
+            cerr << "\ndepth " << depth << " move " << i;
             
             next_pg->pos = next_moves[i];
-            cerr << "pos " << next_pg->pos.first << " " << next_pg->pos.second << "\n";
+            cerr << " (pos " << next_pg->pos.first << " " << next_pg->pos.second << ") ";
 
             score.push_back(compute_score(next_pg->attack_zombies(zombie_pos),
                                           human_count));
 
-            next_pg->zombies_eat(human_pos);
+            if (!next_pg->zombies_eat(human_pos)) {
+                score[i] = GAMEOVER;
+                continue;
+            }
+
+            next_pg->move_zombies();
 
             if (depth > 0) {
                 score[i] += next_pg->compute_most_rewarding_move(depth - 1).second;
+                cerr << "\nscore " << i << " at depth " << depth << " : " << score[i];
             }
         }
 
@@ -144,18 +162,18 @@ class Playground {
         unsigned zombies_killed = 0;
         for (auto z = old_zombie_pos.begin(); z != old_zombie_pos.end(); z++) {
             if (pow(z->first - pos.first, 2) +
-                pow(z->second - pos.second, 2) >= 4000000) {
+                pow(z->second - pos.second, 2) >= GUN_RANGE_SQUARED) {
                     zombie_pos.emplace_back(z->first, z->second);
             } else {
                 zombies_killed += 1;
             }
         }
-        cerr << "zombies killed " << zombies_killed << "\tList of zombies : ";
-        print(zombie_pos);
+        cerr << "List of zombies : ";
+        print(zombie_pos, false);
         return zombies_killed;
     }
 
-    void zombies_eat(list<pair<int, int>> &old_human_pos) {
+    bool zombies_eat(list<pair<int, int>> &old_human_pos) {
         for (auto z = zombie_pos.begin(); z != zombie_pos.end(); z++) {
             for (auto h = old_human_pos.begin(); h != old_human_pos.end();) {
                 if (z->first == h->first && z->second == h->second) {
@@ -171,11 +189,40 @@ class Playground {
         }
         cerr << "List of humans : ";
         print(human_pos); 
+        return (human_count != 0);
+    }
+
+    void move_zombies() {
+        // TODO : compute distance only once and store it
+        for (auto z = zombie_pos.begin(); z != zombie_pos.end(); z++) {
+            int min_distance = pow(HAUT + 1, 2) + pow(LARG + 1, 2);
+            auto closest_human = *human_pos.begin();
+            for (auto h = human_pos.begin(); h != human_pos.end(); h++) {
+                int distance = pow(z->first - h->first, 2) + pow(z->second - h->second, 2);
+                if (distance < min_distance) {
+                    closest_human = *h;
+                    min_distance = distance;
+                }
+            }
+            move_towards(&*z, closest_human, min_distance);
+        }
+    }
+
+    void move_towards(pair<int, int> *zombie, pair<int, int> target, int distance) {
+        pair<int, int> vector = pair<int, int>(target.first - zombie->first, target.second - zombie->second);
+        float ratio = sqrt(pow(ZOMBIE_SPEED, 2) / (float)distance);
+        if (ratio < 1) {
+            zombie->first += (int)(vector.first * ratio);
+            zombie->second += (int)(vector.second * ratio);
+        } else {
+            zombie->first = target.first;
+            zombie->second = target.second;
+        }
     }
 
     pair<unsigned, unsigned> get_max_score() const {
         auto i = max_element(score.begin(), score.end());
-        return pair(distance(score.begin(), i), *i);
+        return pair<int, int>(distance(score.begin(), i), *i);
     }
 };
 
@@ -188,7 +235,7 @@ int main()
         pg->clear();
         cin >> pg->pos.first >> pg->pos.second; cin.ignore();
         cin >> pg->human_count; cin.ignore();
-        for (int i = 0; i < pg->human_count; i++) {
+        for (unsigned i = 0; i < pg->human_count; i++) {
             int human_id;
             int human_x;
             int human_y;
